2b5287069d42329f9b5afd17a785c8f795c9cf02
NegK <82109900+NegK@users.noreply.github.com>
2021-04-08 13:46:46 -0600

    Update setwelcome.js

--- plugins/setwelcome.js
+++ plugins/setwelcome.js
@@ -3,11 +3,11 @@ let handler = async (m, { conn, text, isROwner, isOwner }) => {
     if (isROwner) global.conn.welcome = text
     else if (isOwner) conn.welcome = text
     else global.DATABASE._data.chats.sWelcome = text
-    m.reply('Welcome berhasil diatur\n@user (Mention)\n@subject (Judul Grup)')
-  } else throw 'Teksnya mana?'
+    m.reply('Bievenida exitosa\n@user (Mencion)\n@subject (Titulo del grupo)')
+  } else throw '¿Qué texto?'
 }
-handler.help = ['setwelcome <teks>']
-handler.tags = ['owner', 'group']
+handler.help = ['setwelcome <texto>']
+handler.tags = ['owner', 'grupo']
 
 handler.command = /^setwelcome$/i
 module.exports = handler
