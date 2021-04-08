f6768a00570c9be4e333a492d46b987dbadc5c77
NegK <82109900+NegK@users.noreply.github.com>
2021-04-08 13:14:30 -0600

    Update math_answer.js

--- plugins/math_answer.js
+++ plugins/math_answer.js
@@ -1,21 +1,21 @@
 global.math = global.math ? global.math : {}
 let handler = async (m, { conn }) => {
   let id = m.chat
-  if (!m.quoted || m.quoted.sender != conn.user.jid || !/^Berapa hasil dari/i.test(m.quoted.text)) throw false
-  if (!(id in global.math)) throw 'Soal itu telah berakhir'
+  if (!m.quoted || m.quoted.sender != conn.user.jid || !/^¿Cuál es el resultado de/i.test(m.quoted.text)) throw false
+  if (!(id in global.math)) throw 'Se acabó.'
   if (m.quoted.id == global.math[id][0].id) {
   let math = global.math[id][1]
   if (m.text == math.result) {
-    conn.reply(m.chat, `*Jawaban Benar!*\n+${math.bonus} XP`, m)
+    conn.reply(m.chat, `*¡Respuesta correcta!*\n+${math.bonus} XP`, m)
     global.DATABASE._data.users[m.sender].exp += math.bonus
     clearTimeout(global.math[id][3])
     delete global.math[id]
   } else {
     if (--global.math[id][2] == 0) {
-      conn.reply(m.chat, `*Kesempatan habis!*\nJawaban: *${math.result}*`, m)
+      conn.reply(m.chat, `*¡Se acabaron las posibilidades!*\nrespuesta: *${math.result}*`, m)
       clearTimeout(global.math[id][3])
       delete global.math[id]
-    } else conn.reply(m.chat, `*Jawaban Salah!*\nMasih ada ${global.math[id][2]} kesempatan`, m)
+    } else conn.reply(m.chat, `*Respuesta incorrecta!* Todavía hay ${global.math[id][2]} oportunidades`, m)
   }
  }
 }
