1600a35345ed0f1848aff780eab1c27f9cbdd52e
NegK <82109900+NegK@users.noreply.github.com>
2021-04-08 12:02:23 -0600

    Update banuser.js

--- plugins/banuser.js
+++ plugins/banuser.js
@@ -1,13 +1,13 @@
 let { MessageType } = require('@adiwajshing/baileys')
 let handler = async (m, { conn, text}) => {
-    if (!text) throw 'Siapa yang mau di banned?'
+    if (!text) throw 'A quien quieres prohibir?'
     let who
     if (m.isGroup) who = m.mentionedJid[0]
     else who = m.chat
-    if (!who) throw 'Tag salah satu lah'
+    if (!who) throw 'Etiqueta a uno de ellos'
     let users = global.DATABASE._data.users
     users[who].banned = true
-    conn.reply(m.chat, `berhasil banned`, m)
+    conn.reply(m.chat, `Ha sido prohibido`, m)
 }
 handler.help = ['ban']
 handler.tags = ['owner']
