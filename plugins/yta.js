6ade68edc46686050e23473841b369a717c2984d
NegK <82109900+NegK@users.noreply.github.com>
2021-04-08 16:49:32 -0600

    Update yta.js

--- plugins/yta.js
+++ plugins/yta.js
@@ -1,22 +1,22 @@
 let limit = 30
 const { servers, yta } = require('../lib/y2mate')
 let handler = async (m, { conn, args, isPrems, isOwner }) => {
-  if (!args || !args[0]) throw 'Uhm... Y el URL?'
+  if (!args || !args[0]) throw 'Uhm... urlnya mana?'
   let server = (args[1] || 'id4').toLowerCase()
   let { dl_link, thumb, title, filesize, filesizeF} = await yta(args[0], servers.includes(server) ? server : 'id4')
   let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < filesize
   conn.sendFile(m.chat, thumb, 'thumbnail.jpg', `
-*Titulo:* ${title}
-*Peso:* ${filesizeF}
-*${isLimit ? 'Pakai ': ''}
+*Title:* ${title}
+*Filesize:* ${filesizeF}
+*${isLimit ? 'Pakai ': ''}Link:* ${dl_link}
 `.trim(), m)
-  if (!isLimit) conn.sendFile(m.chat, title + '.mp3', `
-*Titulo:* ${title}
-*Peso:* ${filesizeF}
+  if (!isLimit) conn.sendFile(m.chat, dl_link, title + '.mp3', `
+*Title:* ${title}
+*Filesize:* ${filesizeF}
 `.trim(), m)
 }
 handler.help = ['mp3','a'].map(v => 'yt' + v + ' <url> [server: id4, en60]')
-handler.tags = ['descargas']
+handler.tags = ['downloader']
 handler.command = /^yt(a|mp3)$/i
 handler.owner = false
 handler.mods = false
