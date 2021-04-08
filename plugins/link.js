02ef0dec6bd8ae485e73b75a6cceee4c19992a8f
NegK <82109900+NegK@users.noreply.github.com>
2021-04-08 13:04:25 -0600

    Update link.js

--- plugins/link.js
+++ plugins/link.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn, args }) => {
   conn.reply(m.chat, 'https://chat.whatsapp.com/' + (await conn.groupInviteCode(m.chat)), m)
 }
 handler.help = ['linkgroup']
-handler.tags = ['group']
+handler.tags = ['grupo']
 handler.command = /^link(gro?up)?$/i
 handler.owner = false
 handler.mods = false
