84e61c99d3c42f297f73b1a573066a09108545c9
NegK <82109900+NegK@users.noreply.github.com>
2021-04-08 16:37:14 -0600

    Update sticker.js

--- plugins/sticker.js
+++ plugins/sticker.js
@@ -8,18 +8,17 @@ let handler  = async (m, { conn, args }) => {
     if (/image|video/.test(mime)) {
       let img = await q.download()
       if (!img) throw 'Foto/Video no encontrado'
-      stiker = await sticker(img, false)
-    } else if (args[0]) stiker = await sticker(false, args[0])
+      stiker = await sticker(img, false, global.packname, global.author)
+    } else if (args[0]) stiker = await sticker(false, args[0], global.packname, global.author)
   } finally {
     if (stiker) conn.sendMessage(m.chat, stiker, MessageType.sticker, {
       quoted: m
     })
-    else throw 'Conversion fallida'
+    else throw 'Conversion failed'
   }
 }
-handler.help = ['stiker (subtitulo|marca el multimedia)', 'stiker <url>', 'stikergif (subtitulo|marca el multimedia)', 'stikergif <url>']
+handler.help = ['stiker (caption|reply media)', 'stiker <url>', 'stikergif (caption|reply media)', 'stikergif <url>']
 handler.tags = ['sticker']
 handler.command = /^s(tic?ker)?(gif)?(wm)?$/i
 
 module.exports = handler
-
