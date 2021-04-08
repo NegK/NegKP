cd8fd0fa734a259e0ceaf41ec68be3735a98e2b8
NegK <82109900+NegK@users.noreply.github.com>
2021-04-08 12:18:50 -0600

    Update calc.js

--- plugins/calc.js
+++ plugins/calc.js
@@ -31,7 +31,7 @@ let handler = async (m, { conn, text }) => {
   }
 }
 handler.help = ['calc <expresión>']
-handler.tags = ['tools']
+handler.tags = ['herramientas']
 handler.command = /^(calc(ulat(e|or))?|kalk(ulator)?)$/i
 handler.exp = 5
 
