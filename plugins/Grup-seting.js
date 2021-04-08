1c16456eb92dfb1f9cf31ae0118d3fc8f2d75c72
NegK <82109900+NegK@users.noreply.github.com>
2021-04-08 11:56:49 -0600

    Update Grup-seting.js

--- plugins/Grup-seting.js
+++ plugins/Grup-seting.js
@@ -1,13 +1,13 @@
 let { Presence, GroupSettingChange } = require('@adiwajshing/baileys')
 let handler  = async (m, { conn, args, usedPrefix, command }) => {
-	let isClose = { // Switch Case Like :v
+	let isClose = { 
 		'open': false,
 		'close': true,
 	}[(args[0] || '')]
 	await conn.updatePresence(m.chat, Presence.composing)
 	if (isClose === undefined)
 		throw `
-*Format salah! Contoh :*
+*¡Formato incorrecto!* ejemplo :
 
   *○ ${usedPrefix + command} close*
   *○ ${usedPrefix + command} open*
