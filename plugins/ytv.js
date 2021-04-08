7b827b41146a637a431f2c7a5b711d1f281e82e6
NegK <82109900+NegK@users.noreply.github.com>
2021-04-08 16:50:44 -0600

    Update ytv.js

--- plugins/ytv.js
+++ plugins/ytv.js
@@ -2,24 +2,25 @@ let limit = 30
 let fetch = require('node-fetch')
 const { servers, ytv } = require('../lib/y2mate')
 let handler = async (m, { conn, args, isPrems, isOwner }) => {
-  if (!args || !args[0]) throw 'Uhm... Y el URL?'
+  if (!args || !args[0]) throw 'Uhm... urlnya mana?'
   let server = (args[1] || 'id4').toLowerCase()
-  let { thumb, title, filesize, filesizeF} = await ytv(args[0], servers.includes(server) ? server : 'id4')
+  let { dl_link, thumb, title, filesize, filesizeF} = await ytv(args[0], servers.includes(server) ? server : 'id4')
   let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < filesize
   conn.sendFile(m.chat, thumb, 'thumbnail.jpg', `
-*Titulo:* ${title}
-*Peso:* ${filesizeF}
+*Title:* ${title}
+*Filesize:* ${filesizeF}
+*${isLimit ? 'Pakai ': ''}Link:* ${dl_link}
 `.trim(), m)
   let _thumb = {}
   try { _thumb = { thumbnail: await (await fetch(thumb)).buffer() } }
   catch (e) { }
-  if (!isLimit) conn.sendFile(m.chat, title + '.mp4', `
+  if (!isLimit) conn.sendFile(m.chat, dl_link, title + '.mp4', `
 *Title:* ${title}
 *Filesize:* ${filesizeF}
 `.trim(), m, false, _thumb || {})
 }
 handler.help = ['mp4','v',''].map(v => 'yt' + v + ' <url> [server: id4, en60]')
-handler.tags = ['descargas']
+handler.tags = ['downloader']
 handler.command = /^yt(v|mp4)?$/i
 handler.owner = false
 handler.mods = false
@@ -35,4 +36,3 @@ handler.exp = 0
 handler.limit = true
 
 module.exports = handler
-
