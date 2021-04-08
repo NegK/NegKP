89eb7a82b0b90ac82f3a64809227ec3fd8d5b7aa
NegK <82109900+NegK@users.noreply.github.com>
2021-04-08 13:29:40 -0600

    Update pay.js

--- plugins/pay.js
+++ plugins/pay.js
@@ -1,27 +1,27 @@
 let { MessageType } = require('@adiwajshing/baileys')
 let pajak = 0.02
 let handler = async (m, { conn, text }) => {
-  if (!text) throw 'Masukkan jumlah exp yang akan diberi'
+  if (!text) throw 'Introduzca el número de exps que se entregarán'
   let who
   if (m.isGroup) who = m.mentionedJid[0]
   else who = m.chat
-  if (!who) throw 'Tag salah satu lah'
+  if (!who) throw 'Etiqueta alguien'
   let txt = text.replace('@' + who.split`@`[0], '').trim()
   if (isNaN(txt)) throw 'Hanya angka'
   let xp = parseInt(txt)
   let exp = xp
   let pjk = Math.ceil(xp * pajak)
   exp += pjk
-  if (exp < 1) throw 'Minimal 1'
+  if (exp < 1) throw 'Minimo 1'
   let users = global.DATABASE._data.users
-  if (exp > users[m.sender].exp) throw 'Exp tidak mencukupi untuk mentransfer'
+  if (exp > users[m.sender].exp) throw 'Exp insuficiente para transferir'
   users[m.sender].exp -= exp
   users[who].exp += xp
 
-  m.reply(`(${-xp} XP) + (${-pjk} XP (Pajak 2%)) = ( ${-exp} XP)`)
+  m.reply(`(${-xp} XP) + (${-pjk} XP (impuesto 2%)) = ( ${-exp} XP)`)
   conn.fakeReply(m.chat, `+${xp} XP`, who, m.text)
 }
-handler.help = ['pay @user <amount>']
+handler.help = ['pay @usuario <monto>']
 handler.tags = ['xp']
 handler.command = /^pay$/
 handler.rowner = true
