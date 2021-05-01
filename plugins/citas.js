69a59a5445b0844c6dd406ff83828f954641156b
NegK <82109900+NegK@users.noreply.github.com>
2021-05-01 11:16:59 -0600

    Update citas.js

--- plugins/citas.js
+++ plugins/citas.js
@@ -7,7 +7,7 @@ handler.command = /^(citas)$/i
 handler.owner = false
 handler.mods = false
 handler.premium = false
-handler.group = false
+handler.group = true
 handler.private = false
 
 handler.admin = false
