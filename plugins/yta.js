84f62c6853248cabff1494b39fd957afdc6d7c98
NegK <82109900+NegK@users.noreply.github.com>
2021-05-04 06:47:21 -0600

    Update yta.js

--- plugins/yta.js
+++ plugins/yta.js
@@ -1,7 +1,7 @@
 let limit = 30
 const { servers, yta } = require('../lib/y2mate')
 let handler = async (m, { conn, args, isPrems, isOwner }) => {
-  if (!args || !args[0]) throw 'Uhm... urlnya mana?'
+  if (!args || !args[0]) throw 'Uhm... El URL?'
   let server = (args[1] || 'id4').toLowerCase()
   let { dl_link, thumb, title, filesize, filesizeF} = await yta(args[0], servers.includes(server) ? server : 'id4')
   let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < filesize
@@ -21,7 +21,7 @@ handler.command = /^yt(a|mp3)$/i
 handler.owner = false
 handler.mods = false
 handler.premium = false
-handler.group = false
+handler.group = true
 handler.private = false
 
 handler.admin = false
